66ba683a3cc1f6a31ce3438e21eb101589e40b36
rohitdhakane <rohitdhakane6@gmail.com>
2025-02-07 02:47:44 +0530

    feat(frontend): update dependencies, remove unused CSS files, and integrate design system button component

--- metaverse/apps/frontend/src/App.tsx
+++ metaverse/apps/frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@repo/design-system/components/ui/button';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="f text-red-800">
       <h1 className="text-4xl">Hello, world!</h1>
       <p className="text-lg">Welcome to the Metaverse!</p>
+      <Button>Click me!</Button>
       
     </div>
     </>
--- metaverse/apps/frontend/vite.config.ts
+++ metaverse/apps/frontend/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@repo/design-system': path.resolve(__dirname, '../../packages/design-system')
+    }
+  }
+});
