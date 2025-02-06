d3483effca2e3c91577e10ed1638a971a367abb5
rohitdhakane <rohitdhakane6@gmail.com>
2025-02-06 22:59:12 +0530

    feat(ws): implement WebSocket server with user management and room functionality

---
+++ metaverse/apps/ws/src/RoomManager.ts
@@ -0,0 +1,47 @@
+import type { User } from "./User";
+import type { OutgoingMessage } from "./types";
+
+export class RoomManager {
+    rooms: Map<string, User[]> = new Map();
+    static instance: RoomManager;
+
+    private constructor() {
+        this.rooms = new Map();
+    }
+
+    static getInstance() {
+        if (!RoomManager.instance) {
+            RoomManager.instance = new RoomManager();
+        }
+        return RoomManager.instance;
+    }
+
+    public removeUser(user: User, spaceId: string) {
+        if (!this.rooms.has(spaceId)) {
+            return;
+        }
+        this.rooms.set(spaceId, (this.rooms.get(spaceId)?.filter((u) => u.id !== user.id) ?? []));
+    }
+
+    public addUser(spaceId: string, user: User) {
+        if (!this.rooms.has(spaceId)) {
+            this.rooms.set(spaceId, [user]);
+            return;
+        }
+        this.rooms.set(spaceId, [...(this.rooms.get(spaceId) ?? []), user]);
+    }
+
+    public broadcast(message: OutgoingMessage, user: User, roomId: string) {
+        if (!this.rooms.has(roomId)) {
+            return;
+        }
+        const users = this.rooms.get(roomId);
+        if (users) {
+            for (const u of users) {
+                if (u.id !== user.id) {
+                    u.send(message);
+                }
+            }
+        }
+    }
+}
